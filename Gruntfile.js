ea0b850899e26e5e78b26cf9605e0d071d2e3262
Jarrett <jarrett@gravity.com>
2017-01-12 10:10:11 -0800

    Added a deploy task to Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,16 +12,32 @@ module.exports = function(grunt) {
 					keepalive: true
 				}
 			}
+		},
+
+		exec: {
+			s3sync: {
+				//cmd: "find . -type f -name '*.php' -exec php -l {} ;",
+				cmd: 'aws s3 sync --exclude ".git/*" --exclude "node_modules/*" --exclude "Gruntfile.js" --exclude "package.json" . s3://www.dawnpatrol.vc --delete',
+				callback: function (error, stdout, stderr) {
+					grunt.log.write('stdout: ' + stdout);
+					grunt.log.write('stderr: ' + stderr);
+                
+					if (error !== null) {
+						grunt.log.error('exec error: ' + error);
+					}
+				}
+			}
 		}
 
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-exec');
 
 	// Default task(s).
 	grunt.registerTask('default', ['connect:server']);
 	grunt.registerTask('server', ['connect:server']);
+	grunt.registerTask('deploy', ['exec:s3sync']);
 	grunt.registerTask('dev', [ 'default' ]);
-
 
 };
